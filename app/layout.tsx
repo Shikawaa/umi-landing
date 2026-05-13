c5c3295255c62feb1e79853eeaa55244bea87ef5
Alexandre <alexandreandurand@proton.me>
2026-05-13 14:42:07 +0200

    feat : GTM + GA

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,27 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fr" className={`${sourceSans.variable} ${sourceSerif.variable}`}>
-      <body suppressHydrationWarning className="font-sans text-teal-105 bg-white antialiased">{children}</body>
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-TD92R8S2');
+            `,
+          }}
+        />
+      </head>
+      <body suppressHydrationWarning className="font-sans text-teal-105 bg-white antialiased">
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TD92R8S2" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
+        {children}
+      </body>
     </html>
   );
 }
